90a003b6ecf81f94cf29f298c6190725eb967123
bitkill <bitkill@users.noreply.github.com>
2014-09-14 22:07:17 +0100

    add bootstrap & html

--- server.js
+++ server.js
@@ -1,22 +1,45 @@
-/* global console */
-var 
-	express = require('express'),
-	http = require('http'),	
-	io = require('socket.io').listen(port);
+#!/usr/bin/env node
+"use strict";
 
-/* serve them files */
-var app =  express();
+process.title = 'node-lxmaker';
 
-app.use(express.static(__dirname));
+var wsPort = 8080;
 
-http.createServer(app).listen(80);
+function is_running() {
+    //io.adapter(redis({ host: 'localhost', port: 6379 }));
+    console.log('   BattleShip v1');
+    console.log('	 > running on port ' + wsPort);
+}
 
-io.sockets.on('connection', function(client) {
+var express = require('express'),
+	eapp =  express();
 
-	client.on()
-	// check room
-	// send room details
-	// if room is not full, add user & show on screen
+  	eapp.use(express.static(__dirname + '/public'));
+
+var
+	app = require('http').createServer(eapp).listen(Number(process.env.PORT || wsPort), is_running),
+	io = require('socket.io').listen(app,{origins:'*:*'});
+
+
+var usernames = {};                     // usernames which are currently connected to the chat
+var rooms = ['game','waiting1','waiting2'];  // rooms which are currently available in chat
+
+io.sockets.on('connection', function(socket) {
+
+	socket.on('adduser', function(username) {
+		socket.username = username;
+		usernames[username] = username;
+		console.log( username + ' user has connected!');
+	});
+
+
+	socket.on('disconnect', function() {
+		if (socket.room == 'game') {
+			// reset game, find players
+		}
+
+		// remove the username from global usernames list
+        delete usernames[socket.username];
+        socket.leave(socket.room)
+	});
 });
-
-console.log('-> server is running');
